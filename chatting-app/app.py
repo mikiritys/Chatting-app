4d780d97e516f32b757e4e150478c090ff255488
Alexandr Merunka <sasahrac@gmail.com>
2022-07-17 22:03:41 +0200

    SocketIO

--- chatting-app/app.py
+++ chatting-app/app.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template, session, copy_current_request_context
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit, disconnect
 from threading import Lock
+from flask_ngrok import run_with_ngrok
 
 
-async_mode = None
+async_mode = "threading"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socket_ = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
+run_with_ngrok(app)
 
 
-@app.route('/test')
+@app.route('/')
 def index():
-    return render_template('test.html', async_mode=socket_.async_mode)
+    return render_template('index.html', async_mode=socket_.async_mode)
 
 
-@socket_.on('my_event', namespace='/test')
+@socket_.on('my_event', namespace='/')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': message['data'], 'count': session['receive_count']})
 
 
-@socket_.on('my_broadcast_event', namespace='/test')
+@socket_.on('my_broadcast_event', namespace='/')
 def test_broadcast_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -31,9 +33,8 @@ def test_broadcast_message(message):
          broadcast=True)
 
 
-@socket_.on('disconnect_request', namespace='/test')
+@socket_.on('disconnect_request', namespace='/')
 def disconnect_request():
-    @copy_current_request_context
     def can_disconnect():
         disconnect()
 
@@ -44,4 +45,4 @@ def disconnect_request():
 
 
 if __name__ == '__main__':
-    socket_.run(app, debug=True)
+    app.run()
